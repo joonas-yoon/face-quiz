2e2bb09bfd7f112e87405abf038b005f261f4f3c
joonas <joonas.yoon@gmail.com>
2024-01-18 22:44:43 +0900

    feat: detect faces and draw rectangle

--- assets/site.js
+++ assets/site.js
@@ -1,6 +1,7 @@
 let mainCanvas;
 let context;
 let currentImg;
+let faces;
 
 window.onload = async () => {
   console.log('window onload');
@@ -15,16 +16,19 @@ window.onload = async () => {
     spinner.style.display = 'none';
     isModelLoaded = true;
   }
-  fileElement.addEventListener('change', (evt) => {
+  fileElement.addEventListener('change', async (evt) => {
     if (!isModelLoaded) {
       window.alert('Model not loaded yet');
       return;
     }
-    openImage(evt, async (dataURL) => {
-      currentImg = await faceapi.fetchImage(dataURL);
-      landmarks = await faceapi.detectFaceLandmarks(currentImg);
-      redraw();
-    });
+    const imgFile = evt.target.files[0];
+    currentImg = await faceapi.bufferToImage(imgFile);
+    landmarks = await faceapi.detectFaceLandmarks(currentImg);
+
+    const options = getFaceDetectorOptions();
+    faces = await faceapi.detectAllFaces(currentImg, options);
+
+    redraw();
   });
   inputContainer.addEventListener('click', (evt) => {
     evt.preventDefault();
@@ -32,7 +36,23 @@ window.onload = async () => {
   });
 };
 
-function redraw() {
+function drawCircle(ctx, x, y, color) {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  const rad = 8;
+  ctx.arc(x + rad / 2, y + rad / 2, rad, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function drawPoints(ctx, points, color) {
+  console.log(points);
+  for (const {x, y} of points) {
+    drawCircle(ctx, x, y, color);
+  }
+}
+
+async function redraw() {
   mainCanvas = faceapi.createCanvasFromMedia(currentImg);
   mainCanvas.className = 'uploaded';
   context = mainCanvas.getContext('2d');
@@ -40,6 +60,45 @@ function redraw() {
   console.log(mainCanvas);
   container.innerHTML = '';
   container.appendChild(mainCanvas);
+
+  for (const face of faces) {
+    const cw = mainCanvas.width,
+      ch = mainCanvas.height;
+    const {x, y, width, height} = face.relativeBox;
+    console.log(x, y, width, height);
+    // draw box
+    const oldWidth = context.lineWidth;
+    context.beginPath();
+    context.rect(x * cw, y * ch, width * cw, height * ch);
+    context.strokeStyle = '#0000ff';
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
+    context.lineWidth = oldWidth;
+  }
+
+  // console.log('currentImg', currentImg);
+
+  // drawPoints(
+  //   context,
+  //   [
+  //     ...landmarks.positions.slice(17, 22), // left eyebrow
+  //     ...landmarks.positions.slice(22, 27), // right eyebrow
+  //     ...landmarks.positions.slice(36, 42), // left eye
+  //     ...landmarks.positions.slice(42, 48), // right eye
+  //     ...landmarks.positions.slice(29, 36), // nose
+  //     ...landmarks.positions.slice(48, 68), // mouth
+  //   ],
+  //   '#0000ff88'
+  // );
+
+  // drawPoints(context, [...landmarks.positions.slice(17, 22)], '#0000ff88');
+  // drawPoints(context, [...landmarks.positions.slice(22, 27)], '#00ff0088');
+  // drawPoints(context, [...landmarks.positions.slice(36, 42)], '#ff000088');
+  // drawPoints(context, [...landmarks.positions.slice(42, 48)], '#00ffff88');
+  // drawPoints(context, [...landmarks.positions.slice(29, 36)], '#ff00ff88');
+  // drawPoints(context, [...landmarks.positions.slice(48, 68)], '#ffff0088');
+
   // left eye
   clipAndCopyImage(
     [
@@ -66,6 +125,10 @@ function redraw() {
   clipAndCopyImage(landmarks.positions.slice(29, 36), 640, 0, 640, 640);
   // mouth
   clipAndCopyImage(landmarks.positions.slice(48, 68), 0, 640, 1920, 800);
+}
+
+function clipImage(img, x, y, width, height) {
+
 }
 
 function clipAndCopyImage(points, dx, dy, dw, dh) {
@@ -131,17 +194,26 @@ function getBoundingBox(points) {
 }
 
 async function loadModel() {
-  await faceapi.loadFaceLandmarkModel(
-    'https://cdn.jsdelivr.net/gh/justadudewhohacks/face-api.js@0.22.2/weights/'
-  );
+  const url =
+    'https://cdn.jsdelivr.net/gh/justadudewhohacks/face-api.js@0.22.2/weights/';
+  await faceapi.loadFaceLandmarkModel(url);
+  await faceapi.loadSsdMobilenetv1Model(url);
 }
 
-function openImage(file, callback) {
-  const input = file.target;
-  const reader = new FileReader();
-  reader.onload = function () {
-    const dataURL = reader.result;
-    callback(dataURL);
-  };
-  reader.readAsDataURL(input.files[0]);
+const SSD_MOBILENETV1 = 'ssd_mobilenetv1';
+const TINY_FACE_DETECTOR = 'tiny_face_detector';
+
+let selectedFaceDetector = SSD_MOBILENETV1;
+
+// ssd_mobilenetv1 options
+let minConfidence = 0.5;
+
+// tiny_face_detector options
+let inputSize = 512;
+let scoreThreshold = 0.5;
+
+function getFaceDetectorOptions() {
+  return selectedFaceDetector === SSD_MOBILENETV1
+    ? new faceapi.SsdMobilenetv1Options({minConfidence})
+    : new faceapi.TinyFaceDetectorOptions({inputSize, scoreThreshold});
 }
